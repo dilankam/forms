4b6e706963dc2204223eb081cff68b5b143bfb25
dilankatest <dilanka.test@gmail.com>
2019-01-24 14:17:54 +0530

    Image preview after upload

--- src/app/components/create-post/create-post.component.ts
+++ src/app/components/create-post/create-post.component.ts
@@ -13,6 +13,7 @@ export class CreatePostComponent implements OnInit {
   enteredTitle="";
   enteredContent="";
   titlePlceHolder="Title";
+  imagePreview: string;
   private mode = "create";
   private postID: string;
   post:Post;
@@ -27,7 +28,10 @@ export class CreatePostComponent implements OnInit {
       }),
       'enteredContent' : new FormControl(null,{
         validators:[Validators.required, Validators.minLength(3)]
-      })
+      }),
+      // 'image' is not bind to any html element
+      // in reactive forms it's not necessary to bind with html form  
+      'image': new FormControl(null, {validators: [Validators.required]})
     });
 
     //this should be a observable because url changing
@@ -40,7 +44,7 @@ export class CreatePostComponent implements OnInit {
         this.postService.getPost(this.postID).subscribe(postData=>{
           this.post = {id:postData._id, title:postData.title,content:postData.content};
         
-          //set value to the form on edit
+          //set value to the form on edit (html form)
           this.form.setValue({
             'enteredTitle': this.post.title,
             'enteredContent': this.post.content
@@ -52,6 +56,22 @@ export class CreatePostComponent implements OnInit {
       }
     });
     
+  }
+
+  onImagePicked(event:Event){
+    // angular doesn't know our target is file field so convert it to a HTMLInputElement
+    //fieles are array of files
+    const file = (event.target as HTMLInputElement).files[0];
+    // add selected image to 'image' field in the form object
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    
+    // pass image file object to file reader, read file and assign it to imagePreview
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
   }
 
   onSavePost(){
